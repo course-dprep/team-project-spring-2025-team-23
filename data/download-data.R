35241de7ba7892b6f97e745c0eadea529c9d8fff
jtorralbaflores <78760253+jtorralbaflores@users.noreply.github.com>
2025-02-17 18:07:59 +0100

    Adding instructions on how to load data, deliverable from week 2 (pending corrections to it)

--- data/download-data.R
+++ data/download-data.R
@@ -1,2 +1,15 @@
 # This script will be used to populate the \data directory
 # with all necessary raw data files.
+
+# Links:
+
+url_1 <- "https://drive.google.com/uc?export=download&id=1MshPpoxwfBoTabDyqMRpPMOFMhuEnGMY"
+mkt_share_no_tech <- read.csv(url_1)
+
+url_2 <- "https://drive.google.com/uc?export=download&id=1OGLOgJOIKeRyX-6QVvW1IRzkNAeAGwzU"
+cbsa_urban_rural <- read.csv(url_2)
+
+url_3 <- "https://drive.google.com/uc?export=download&id=1cDJjXLIdZxxq3TR1_wqH2TVjdG19LiUx"
+mkt_share_w_tech <- read.csv(url_3)
+
+
